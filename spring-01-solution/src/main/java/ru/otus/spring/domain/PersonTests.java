4a381946d687042f84e804274c5730a982d01ba2
enextus <enextus@gmail.com>
2022-03-01 17:35:16 +0100

    01.03.2022 02

---
+++ spring-01-solution/src/main/java/ru/otus/spring/domain/PersonTests.java
@@ -0,0 +1,22 @@
+package ru.otus.spring.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTests {
+
+	@org.junit.jupiter.api.BeforeEach
+	void setUp() {
+	}
+
+	@org.junit.jupiter.api.AfterEach
+	void tearDown() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void getName() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void getAge() {
+	}
+}
