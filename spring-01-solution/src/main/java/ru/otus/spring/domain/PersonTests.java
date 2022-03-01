f7deded6f72d92fc091a08e2a1ac13459a589e5f
enextus <enextus@gmail.com>
2022-03-01 18:15:52 +0100

    01.03.2022 03

--- spring-01-solution/src/main/java/ru/otus/spring/domain/PersonTests.java
+++ spring-01-solution/src/main/java/ru/otus/spring/domain/PersonTests.java
@@ -1,22 +1,50 @@
 package ru.otus.spring.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Tests of Class Person")
 class PersonTests {
 
-	@org.junit.jupiter.api.BeforeEach
+	private Person person;
+
+	@BeforeEach
 	void setUp() {
+
+		String a = "Ivan";
+		int b = 18;
+
+		person = new Person(a, b);
+		System.out.println("Test started.");
 	}
 
-	@org.junit.jupiter.api.AfterEach
+	@AfterEach
 	void tearDown() {
+		System.out.println("Test done.");
 	}
 
-	@org.junit.jupiter.api.Test
-	void getName() {
+	@DisplayName("the constructor works correct")
+	@Test
+	void shouldHaveCorrectConstructor() {
+		assertEquals("Ivan", person.getName());
+		assertEquals(18, person.getAge());
 	}
 
-	@org.junit.jupiter.api.Test
+	@DisplayName("the getName method works correct")
+	@Test
+	void getName_01() {
+		assertEquals("Ivan", person.getName());
+	}
+
+	@DisplayName("the getAge method works correct")
+	@Test
 	void getAge() {
+		assertEquals(18, person.getAge());
 	}
 }
