9b6d699b808f18f4aa191e58533663571a42bf5a
enextus <enextus@gmail.com>
2022-03-03 13:46:16 +0100

    context + model (domain)

--- spring-01-exercise/src/main/java/ru/otus/spring/domain/Person.java
+++ spring-01-exercise/src/main/java/ru/otus/spring/domain/Person.java
@@ -1,13 +1,18 @@
 package ru.otus.spring.domain;
 
 public class Person {
-
+    private final String sex;
     private final String name;
     private final int age;
 
-    public Person(String name, int age) {
+    public Person(String sex, String name, int age) {
+        this.sex = sex;
         this.name = name;
         this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
     }
 
     public String getName() {
--- spring-01-solution/src/main/java/ru/otus/spring/Main.java
+++ spring-01-solution/src/main/java/ru/otus/spring/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+
         PersonService service = context.getBean(PersonService.class);
         Person ivan = service.getByName("Ivan");
         System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
